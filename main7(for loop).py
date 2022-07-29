dae9668b6c84c154371baac79b340b798ef18f72
Kiran Rai <xorayeez@gmail.com>
2022-07-29 16:10:30 +0100

    Here is some practice that i did today lot more to go and guess what i learnt for loop not to the depth but on the surface

---
+++ main7(for loop).py
@@ -0,0 +1,15 @@
+# list1 = ["Kiran","Chiran","Diran","Firan"]
+# for item in list1:
+#     print(item)
+
+# list of list
+# list1 = [ ["Kiran", 1],["Chiran", 2],["Diran", 3], ["Miran", 4] ]
+# dict1 = dict(list1)
+# for item, lollypop in dict1.items():
+#     print(item,"and lolly is", lollypop)
+
+# items = [int, float, "Kiran",5,3,4,6,7,8,9,34,234,23,4,2,34]
+# for item in items:
+#     if str(item).isnumeric() and item>6:
+#         print(item)
+
